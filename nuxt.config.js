0271afd8c90ad16a9d78411ed3367b1c3f328a24
toshiki-murakami <murakami.toshiki1230@gmail.com>
2020-03-22 22:40:20 +0900

    .

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,8 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
-    "@/plugins/firebase"
+    "@/plugins/firebase",
+    "@/plugins/vee-validate"
   ],
   /*
   ** Nuxt.js dev-modules
