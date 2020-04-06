89bf12be7ff1eb1869e97370f37e128d29cb51d7
toshiki-murakami <murakami.toshiki1230@gmail.com>
2020-04-06 22:50:31 +0900

    .

--- store/index.js
+++ store/index.js
@@ -57,6 +57,31 @@ export const actions = {
         commit("setIsLoading", false);
         commit("setError", error);
       });
+  },
+
+  loginUser({ commit }, payload) {
+    commit("setIsLoading", true);
+    commit("clearError");
+
+    this.$fireApp
+      .auth()
+      .signInWithEmailAndPassword(payload.email, payload.password)
+      .then(data => {
+        const authUser = {
+          id: data.user.uid,
+          email: data.user.email,
+          name: data.user.displayName
+        };
+        commit("setUser", authUser);
+        commit("setJobDone", true)
+        commit("setIsLoading", false);
+      })
+      .catch(error => {
+        commit("setBusy", false);
+        commit("setError", error);
+      })
+
+
   }
 };
 
